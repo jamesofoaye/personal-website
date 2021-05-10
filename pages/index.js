6ddca5f292c9cf752ea9e8bfa75cc382119d97d9
James Ofori Ayerakwa <58654469+jamesofoaye@users.noreply.github.com>
2021-05-10 12:22:26 +0000

    Removed shadow from the image

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function IndexPage() {
       <Nav />
       <div className="max-w-4xl flex items-center flex-wrap mx-auto lg:my-0 lg:pb-12">
         <div className="w-full  lg:w-2/5">
-          <img src="./mypic.jpg" className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block" />
+          <img src="./mypic.jpg" className="rounded-none lg:rounded-lg hidden lg:block" />
         </div>
         <div className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl opacity-75 mx-6 mt-8 lg:mt-0 lg:mx-0">
           <div className="p-4 md:p-12 text-center lg:text-left">

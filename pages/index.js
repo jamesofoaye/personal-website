df93bbc47ae32777e2694d579803ea5469af8c62
James Ofori Ayerakwa <jamesofoaye@gmail.com>
2021-11-11 17:13:12 +0000

    fixed image position

--- pages/index.js
+++ pages/index.js
@@ -12,30 +12,33 @@ export default function IndexPage() {
       </Head>
 
       <main className={`relative h-screen ${styles.bgImage}`}>
-        <header className="h-24 sm:h-32 flex items-center z-30 w-full">
+        <nav className="h-24 sm:h-32 flex items-center z-30 w-full">
           <div className="container mx-auto px-6 flex items-center justify-between">
             <div className="uppercase text-gray-800 font-semibold text-xl flex items-center">
               Ofori James Ayerakwa
             </div>
             <div className="flex items-center">
-              <nav className="font-sen text-gray-800 uppercase text-lg lg:flex items-center hidden">
-                <div className="uppercase text-gray-800 text-3xl font-semibold flex items-center">
-                  <svg width="25" height="25" viewBox="0 0 1792 1792" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1664 1504v-768q-32 36-69 66-268 206-426 338-51 43-83 67t-86.5 48.5-102.5 24.5h-2q-48 0-102.5-24.5t-86.5-48.5-83-67q-158-132-426-338-37-30-69-66v768q0 13 9.5 22.5t22.5 9.5h1472q13 0 22.5-9.5t9.5-22.5zm0-1051v-24.5l-.5-13-3-12.5-5.5-9-9-7.5-14-2.5h-1472q-13 0-22.5 9.5t-9.5 22.5q0 168 147 284 193 152 401 317 6 5 35 29.5t46 37.5 44.5 31.5 50.5 27.5 43 9h2q20 0 43-9t50.5-27.5 44.5-31.5 46-37.5 35-29.5q208-165 401-317 54-43 100.5-115.5t46.5-131.5zm128-37v1088q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-1088q0-66 47-113t113-47h1472q66 0 113 47t47 113z">
-                    </path>
-                  </svg>
-                  <span className="text-xl ml-3 mt-1">
-                    jamesofoaye@gmail.com
-                  </span>
+              <a href="mailto:jamesofoaye@gmail.com?subject=Hi James, Found you via your website☺">
+                <div className="font-sen text-gray-800 uppercase text-lg flex items-center">
+                  <div className="uppercase text-gray-800 text-3xl font-semibold flex items-center">
+                    <svg width="25" height="25" viewBox="0 0 1792 1792" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M1664 1504v-768q-32 36-69 66-268 206-426 338-51 43-83 67t-86.5 48.5-102.5 24.5h-2q-48 0-102.5-24.5t-86.5-48.5-83-67q-158-132-426-338-37-30-69-66v768q0 13 9.5 22.5t22.5 9.5h1472q13 0 22.5-9.5t9.5-22.5zm0-1051v-24.5l-.5-13-3-12.5-5.5-9-9-7.5-14-2.5h-1472q-13 0-22.5 9.5t-9.5 22.5q0 168 147 284 193 152 401 317 6 5 35 29.5t46 37.5 44.5 31.5 50.5 27.5 43 9h2q20 0 43-9t50.5-27.5 44.5-31.5 46-37.5 35-29.5q208-165 401-317 54-43 100.5-115.5t46.5-131.5zm128-37v1088q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-1088q0-66 47-113t113-47h1472q66 0 113 47t47 113z">
+                      </path>
+                    </svg>
+                    <span className="text-xl ml-3 mt-1">
+                      jamesofoaye@gmail.com
+                    </span>
+                  </div>
                 </div>
-              </nav>
+              </a>
             </div>
           </div>
-        </header>
+        </nav>
+
         <div className="flex relative z-20 items-center">
-          <div className="container mx-auto px-6 flex flex-col justify-between items-center relative py-4">
+          <div className="container mx-auto px-6 flex flex-col justify-between items-center relative pt-4">
             <div className="flex flex-col">
-              <img src="/new-pic.jpg" className="rounded-full w-24 h-24 mx-auto" />
+              <img src="/new-pic.jpg" className="rounded-full h-40 w-40 mx-auto" />
               <p className="text-6xl my-6 font-bold text-center">
                 Hi, I&apos;m James
               </p>
@@ -123,7 +126,8 @@ export default function IndexPage() {
             </div>
           </div>
         </div>
-              </div>*/}
+              </div>*/
+      }
     </div>
   )
 }

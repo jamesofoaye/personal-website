62a787bafbbdebe8c20c1d012c1adf5b114b26c8
James Ofori Ayerakwa <58654469+jamesofoaye@users.noreply.github.com>
2022-10-11 00:38:24 +0400

    feat: installed chakra UI and removed tailwind css

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
-import '../styles/index.css'
-import 'tailwindcss/tailwind.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+	brand: {
+		black: "#0E1017",
+		yellow: "#E6AF2E",
+	},
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Component {...pageProps} />
-  )
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;

e369613017b67f50c66263529b4249494419bccd
Ofori James Ayerakwa <jamesofoaye@gmail.com>
2022-10-17 08:30:06 +0400

    chore: removed google fonts links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,14 +9,7 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-					<link
-						href="https://fonts.googleapis.com/css2?family=Commissioner:wght@100;200;300;400;500;600;700;800;900&display=swap"
-						rel="stylesheet"
-					/>
-				</Head>
+				<Head></Head>
 				<body>
 					<Main />
 					<NextScript />
